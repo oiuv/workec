b419613b8fd57e630c442663b012e5372cc32ff2
oiuv <i@oiuv.cn>
2020-06-20 21:14:40 +0800

    update to v2

--- README.md
+++ README.md
@@ -28,67 +28,85 @@ $ec = new EC('corpId', 'appId', 'appSecret');
 echo $ec->structure();
 ```
 
-> 获取指定员工信息
-```php
-$mobile = '13800138000';
-
-echo $ec->findUserInfoById($mobile);
-```
-
-> 批量精确查询客户
+> 查询客户
 ```php
 //通过手机号查询单个客户
-$data = '13800138000';
-
-//通过手机号批量查询客户
-$data = ['13800138000', '13900139000'];
+$mobile = '13800138000';
+echo $ec->queryCustomer($mobile);
 
 //通过crmId批量查询客户
-$data = [['crmId' => 123], ['crmId' => 456], ['crmId' => 789]]
-
-echo $ec->getCustomer($data);
+$crmIds = '12345,14336,13093';
+echo $ec->preciseQueryCustomer($crmIds);
 ```
 
 > 创建客户
 ```php
-//更多参数见文档：https://open.workec.com/apidoc/index.html#api-groupCustomer-6
+// 单个创建；参数：$optUserId, $name, $mobile, $followUserId
+echo $ec->addCustomer(12345, '测试API', '13800138000', 67890);
+// 批量创建
 $data = [
-    'optUserId' => '123456',
-    'f_name'    => '测试API',
-    'f_mobile'  => '13800138000',
-    'f_channel' => '直接输入',
-    'f_memo'    => 'API创建客户'
+    'optUserId' => $optUserId,
+    'list' => [
+        [
+            'name' => $name,
+            'mobile' => $mobile,
+            'followUserId' => $followUserId,
+            'channelId' => $channelId,
+            'memo' => $memo,
+        ],
+        [
+            'name' => $name,
+            'mobile' => $mobile,
+            'followUserId' => $followUserId,
+            'channelId' => $channelId,
+            'memo' => $memo,
+        ],
+        [
+            'name' => $name,
+            'mobile' => $mobile,
+            'followUserId' => $followUserId,
+            'channelId' => $channelId,
+            'memo' => $memo,
+        ]
+    ]
 ];
-
-echo $ec->addCustomer($data);
+echo $ec->addCustomers($data);
 ```
 
 > 修改客户资料
 ```php
-//更多参数见文档：https://open.workec.com/apidoc/index.html#api-groupCustomer-13
+// 修改单个用户
 $data = [
-    'optUserId' => '123456',
-    'crmId'     => '123456789',
-    'f_name'    => '陈小萌',
-    'f_step'    => 4
+    'optUserId' => 123456,
+    'crmId'     => 123456789,
+    'name'    => '陈小萌',
+    'mobile'    => '13800138000'
 ];
-
 echo $ec->updateCustomer($data);
-```
-
-> 导出历史电话记录
-```php
-//更多参数见文档：https://open.workec.com/apidoc/index.html#api-groupContactRecode-28
-$data = [
-    'year'  => 2018,
-    'month' => 05,
+// 批量修改用户
+$list =[
+    [
+        'optUserId' =>12345,
+        'crmId'=>1234567,
+        'name'=>'用户1'
+    ],
+    [
+        'optUserId' =>12345,
+        'crmId'=>1234568,
+        'name'=>'用户2'
+    ],
+    [
+        'optUserId' =>67890,
+        'crmId'=>1234569,
+        'name'=>'用户3'
+    ]
 ];
-
-echo $ec->telRecordHistory($data);
+echo $ec->batchUpdateCustomer($list);
 ```
+
 > 更多方法看源码并参考EC开放平台技术文档
 
-- [EC开放平台技术文档](https://open.workec.com/apidoc/index.html)
+- [EC开放平台技术文档](https://open.workec.com/newdoc/)
 
 ## License
 
--- src/EC.php
+++ src/EC.php
@@ -2,8 +2,8 @@
 /**
  * EC开放平台API
  * @author oiuv <i@oiuv.cn>
- * @version 1.1.2
- * @link https://open.workec.com/apidoc/index.html
+ * @version 2.0.1
+ * @link https://open.workec.com/newdoc/
  */
 
 namespace Oiuv\WorkEc;
@@ -39,7 +39,7 @@ class EC
 
     public function __construct($corpId = '', $appId = '', $appSecret = '')
     {
-        $this->client = new Client(['base_uri' => 'https://open.workec.com/']);
+        $this->client = new Client(['base_uri' => 'https://open.workec.com/v2/']);
 
         if ($corpId)
             $this->corpId = trim($corpId);
@@ -55,12 +55,17 @@ class EC
 
     public function client($method, $uri, $data = [])
     {
+        // 获取当前时间戳
+        $timeStamp = time() * 1000;
+        // 获取签名
+        $sign = $this->getSign($timeStamp, $this->appId, $this->appSecret);
         try {
             $response = $this->client->request($method, $uri, [
                 'headers' => [
-                    'authorization' => $this->accessToken(),
-                    'corp_id'       => $this->corpId,
-                    'cache-control' => 'no-cache'
+                    'Content-Type' => 'application/json; charset=utf-8',
+                    'X-Ec-Cid' => $this->corpId,
+                    'X-Ec-Sign' => $sign,
+                    'X-Ec-TimeStamp' => $timeStamp
                 ],
                 'json'    => $data,
             ]);
@@ -71,51 +76,167 @@ class EC
     }
 
     /**
-     *  获取access_token
+     *  签名算法
+     * @param int $timeStamp
+     * @param string $appId
+     * @param string $appSecret
+     * @return string 返回签名数据
      */
-    public function accessToken()
+    function getSign($timeStamp, $appId, $appSecret)
     {
-        $response = $this->client->post('auth/accesstoken', ['json' => ['appId' => $this->appId, 'appSecret' => $this->appSecret]]);
-        //return $response->getBody()->getContents();
-        $result = json_decode($response->getBody()->getContents());
-        if ($result->errCode == 200)
-            return $result->data->accessToken;
-        else
-            //return $result->errMsg;
-            return false;
+        $sign = "appId={$appId}&appSecret={$appSecret}&timeStamp={$timeStamp}";
+        return strtoupper(md5($sign));
     }
 
+    /**********客户相关接口**********/
+
     /**
-     *  获取部门和员工信息
+     * 修改客户标签（支持批量）
      */
-    public function structure()
+    public function updateLabel($optUserId, $crmIds, $labels = null, $type = 0)
     {
-        $response = $this->client('get', 'user/structure');
+        $data = [
+            'optUserId' => $optUserId,
+            'crmIds' => $crmIds,
+            'labels' => $labels,
+            'type' => $type,
+        ];
+        $response = $this->client('post', 'label/update', $data);
         return $response;
     }
 
     /**
-     *  获取指定员工信息
-     * @param String $account 用户账号(手机号码)
-     * @param String $userId 用户ID
-     * $userId和$account必须填写一个，如果都填，以$userId为准
-     * @return string
+     * 查询客户标签(支持批量)
+     * @param string $crmIds 客户id列表，多个id使用英文逗号分隔
      */
-    public function findUserInfoById($account = '', $userId = '')
+    public function queryLabel($crmIds)
     {
         $data = [
-            'userId'  => $userId,
-            'account' => $account
+            'crmIds' => $crmIds,
         ];
-        $response = $this->client('get', 'user/findUserInfoById', $data);
+        $response = $this->client('post', 'customer/queryLabel', $data);
+        return $response;
+    }
+
+    /**
+     * 放弃客户(支持批量)
+     */
+    public function abandonCustomer($optUserId, $crmIds, $type = 1)
+    {
+        $data = [
+            'optUserId' => $optUserId,
+            'crmIds' => $crmIds,
+            'type' => $type,
+        ];
+        $response = $this->client('post', 'customer/change/abandon', $data);
+        return $response;
+    }
+
+    /**
+     * 变更跟进人(支持批量)
+     */
+    public function customerChangeUser($optUserId, $crmIds, $followUserId)
+    {
+        $data = [
+            'optUserId' => $optUserId,
+            'crmIds' => $crmIds,
+            'followUserId' => $followUserId,
+        ];
+        $response = $this->client('post', 'customer/change/user', $data);
+        return $response;
+    }
+
+    /**
+     *  查询客户分组(请求协议错误！???)
+     */
+    public function getCustomerGroup($userId)
+    {
+        $data = [
+            'userId' => $userId,
+        ];
+        $response = $this->client('get', 'customer/getCustomerGroup', $data);
+        return $response;
+    }
+
+    /**
+     * 查询客户轨迹
+     */
+    public function getTrajectory($startTime, $endTime, $trajectoryType, $crmIds = '', $lastId = 0, $lastTime = '', $pageSize = 200)
+    {
+        $data = [
+            'date' => [
+                'endTime' => $endTime,
+                'startTime' => $startTime,
+            ],
+            'trajectoryType' => $trajectoryType,
+            'crmIds' => $crmIds,
+            'lastId' => $lastId,
+            'lastTime' => $lastTime,
+            'pageSize' => $pageSize,
+        ];
+        $response = $this->client('post', 'customer/getTrajectory', $data);
+        return $response;
+    }
+
+    /**
+     * 分页查询客户信息
+     */
+    public function queryCustomer($mobile = null, $email = null, $step = null, $labelIds = null, $followUserId = null, $publicPondId = null, $modifyTime = null, $contactTime = null, $createTime = null, $sortField = null,$sortType = null, $pageSize = 200, $pageNo = 1)
+    {
+        $data = [
+            'mobile' => $mobile,
+            'email'  => $email,
+            'step' => $step,
+            'labelIds' => $labelIds,
+            'followUserId' => $followUserId,
+            'publicPondId' => $publicPondId,
+            'modifyTime' => $modifyTime,
+            'contactTime' => $contactTime,
+            'createTime' => $createTime,
+            'orderBy' => [
+                'sortField' => $sortField,
+                'sortType' => $sortType,
+            ],
+            'pageInfo' => [
+                'pageSize' => $pageSize,
+                'pageNo' => $pageNo,
+            ],
+        ];
+        $response = $this->client('post', 'customer/query', $data);
+        return $response;
+    }
+
+    /**
+     * 批量查询客户信息
+     */
+    public function preciseQueryCustomer($crmIds)
+    {
+        $data = [
+            'crmIds' => $crmIds,
+        ];
+        $response = $this->client('post', 'customer/preciseQuery', $data);
         return $response;
     }
 
     /**
      * 创建客户
      */
-    public function addCustomer($data)
+    public function addCustomer($optUserId, $name, $mobile, $followUserId = null, $channelId = null, $memo = '', array $detail = [])
     {
+        $data = [
+            'optUserId' => $optUserId,
+            'list' => [
+                [
+                    'name' => $name,
+                    'mobile' => $mobile,
+                    'followUserId' => $followUserId,
+                    'channelId' => $channelId,
+                    'memo' => $memo,
+                ]
+            ]
+        ];
+        $data['list'][0] += $detail;
+
         $response = $this->client('post', 'customer/addCustomer', $data);
         return $response;
     }
@@ -123,352 +244,108 @@ class EC
     /**
      * 批量创建客户
      */
-    public function createCustomer($data)
-    {
-        $response = $this->client('post', 'customer/create', $data);
-        return $response;
-    }
-
-    /**
-     * 批量精确查询客户
-     * @param array | String $data 参数可以是手机号、手机号数组，或crmId二维数组：[['crmId' => 123], ['crmId' => 456], ['crmId' => 789]]
-     * @return string
-     */
-    public function getCustomer($data)
-    {
-        if (is_array($data)) {
-            if (array_key_exists('crmId', $data[0]))
-                $data = [
-                    'list' => $data
-                ];
-            else {
-                $data = array_map(function ($mobile) {
-                    return [
-                        'mobile' => $mobile
-                    ];
-                }, $data);
-                $data = [
-                    'list' => $data
-                ];
-            }
-        } else
-            $data = [
-                'list' => [
-                    ['mobile' => $data]
-                ]
-            ];
-        $response = $this->client('get', 'customer/get', $data);
-        return $response;
-    }
-
-    /**
-     *  根据条件分页查询客户
-     */
-    public function rangeQueryCustomer($data)
-    {
-        $response = $this->client('post', 'customer/rangeQueryCustomer', $data);
-        return $response;
-    }
-
-    /**
-     * 获取自定义字段信息
-     * @param Int $type 按资料类型传对应值： 1 客户资料 2 公司资料
-     * @return string
-     */
-    public function getCustomFieldMapping($type = 1)
+    public function addCustomers($optUserId, array $list)
     {
         $data = [
-            'type' => $type
+            'optUserId' => $optUserId,
+            'list' => $list,
         ];
-        $response = $this->client('post', 'customer/getCustomFieldMapping', $data);
+
+        $response = $this->client('post', 'customer/addCustomer', $data);
         return $response;
     }
 
     /**
-     * 获取员工客户库分组信息
-     * @param int $userId 员工ID
-     * @return string
+     * 合并客户
      */
-    public function getCustomerGroup($userId)
+    public function combineCustomers($optUserId, array $detail)
     {
         $data = [
-            'userId' => $userId
+            'optUserId' => $optUserId,
         ];
-        $response = $this->client('post', 'customer/getCustomerGroup', $data);
+        $data += $detail;
+
+        $response = $this->client('post', 'customer/combine', $data);
         return $response;
     }
 
     /**
-     * 修改客户资料
+     * 修改客户信息
      */
-    public function updateCustomer($data)
+    public function updateCustomer($optUserId, $crmId, array $detail)
     {
+        $data = [
+            'optUserId' => $optUserId,
+            'crmId' => $crmId,
+        ];
+        $data += $detail;
+
         $response = $this->client('post', 'customer/updateCustomer', $data);
         return $response;
     }
 
     /**
-     * 获取客户来源信息
+     * 批量修改客户信息
      */
-    public function getChannelSource()
+    public function batchUpdateCustomer(array $data)
     {
-        $response = $this->client('get', 'customer/getChannelSource');
+        $response = $this->client('post', 'customer/batchUpdateCustomer', $data);
         return $response;
     }
 
     /**
-     * 变更客户跟进人
+     * 修改客户阶段(支持批量)
      */
-    public function changeCrmFollowUser($data)
-    {
-        $response = $this->client('post', 'customer/changeCrmFollowUser', $data);
-        return $response;
-    }
-
-    /**
-     * 放弃客户
-     */
-    public function abandonCustomer($data)
-    {
-        $response = $this->client('post', 'customer/abandon', $data);
-        return $response;
-    }
-
-    /**
-     * 获取删除的客户
-     * @param String $startTime 查询删除客户的开始时间,格式yyyy-MM-dd HH:mm:ss
-     * @param String $endTime 查询删除客户的截止时间,格式yyyy-MM-dd HH:mm:ss, 与startTime最大间隔7天
-     * @param String $lastId 根据此参数来进行翻页。上一次请求得到的最后一条记录中的id，初始值可为""
-     * @return string
-     */
-    public function delcrms($startTime = '', $endTime = '', $lastId = '')
+    public function updateStep($optUserId, $crmIds, $step)
     {
         $data = [
-            "startTime" => $startTime,
-            "endTime"   => $endTime,
-            "lastId"    => $lastId
+            'optUserId' => $optUserId,
+            'crmIds' => $crmIds,
+            'step' => $step,
         ];
-        $response = $this->client('post', 'customer/delcrms', $data);
+        $response = $this->client('post', 'step/update', $data);
         return $response;
     }
+    /**********异步任务相关接口**********/
+
+    /**********机器人相关接口**********/
+
+    /**********组织架构相关接口**********/
 
     /**
-     * 获取员工签到记录
+     *  获取部门和员工信息
      */
-    public function getCrmVisitDetails($data)
+    public function structure()
     {
-        $response = $this->client('post', 'customer/getCrmVisitDetails', $data);
+        $response = $this->client('get', 'org/struct/info');
         return $response;
     }
 
-    /**
-     * 创建标签分组
-     * @param int $userId 操作人ID
-     * @param String $name 标签分组名
-     * @param String $color 分组颜色 默认值为 c1,取值范围[c1~c20]
-     * @param int $type 分组类型 默认值为0 取值： 0 代表此分组的标签可以多选 1 代表此分组的标签只能单选
-     * @return string
-     */
-    public function addLabelGroup($userId, $name, $color = 'c1', $type = 0)
-    {
-        $data = [
-            'name'   => $name,
-            'type'   => $type,
-            'color'  => $color,
-            'userId' => $userId
-        ];
-        $response = $this->client('post', 'label/addLabelGroup', $data);
-        return $response;
-    }
+    /**********记录相关接口**********/
 
     /**
-     * 创建标签
-     * @param String $name 标签名
-     * @param String $groupValue 分组id或者分组名
-     * @param int $userId 操作人ID
-     * @return string
+     * 电话空闲用户
      */
-    public function addLabel($name, $groupValue, $userId)
+    public function getFreeStatusUid()
     {
         $data = [
-            'name'        => $name,
-            '$groupValue' => $groupValue,
-            'userId'      => $userId
+            'userState'  => 1
         ];
-        $response = $this->client('post', 'label/addLabel', $data);
+        $response = $this->client('post', 'record/getFreeStatusUid', $data);
         return $response;
     }
 
-    /**
-     * 批量修改客户标签
-     */
-    public function updateLabel($data)
-    {
-        $data = [
-            'list' => $data
-        ];
-        $response = $this->client('post', 'label/update', $data);
-        return $response;
-    }
+    /**********配置相关接口**********/
 
     /**
      * 获取标签信息
-     * @param String $groupValue 分组id或者分组名
-     * @return string
      */
-    public function getLabelInfo($groupValue = '')
+    public function getLabelInfo()
     {
-        $data = [
-            'groupValue' => $groupValue
-        ];
-        $response = $this->client('post', 'label/getLabelInfo', $data);
+        $response = $this->client('get', 'config/getLabelInfo');
         return $response;
     }
 
-    /**
-     * 批量添加跟进记录
-     */
-    public function saveUserTrajectory($data)
-    {
-        $data = [
-            'list' => $data
-        ];
-        $response = $this->client('post', 'trajectory/saveUserTrajectory', $data);
-        return $response;
-    }
+    /**********统计相关接口**********/
 
-    /**
-     * 导出跟进记录
-     */
-    public function findUserTrajectory($data)
-    {
-        $response = $this->client('post', 'trajectory/findUserTrajectory', $data);
-        return $response;
-    }
-
-    /**
-     * 导出历史跟进记录
-     */
-    public function findHistoryUserTrajectory($data)
-    {
-        $response = $this->client('post', 'trajectory/findHistoryUserTrajectory', $data);
-        return $response;
-    }
-
-    /**
-     * 导出电话记录
-     */
-    public function telRecord($data)
-    {
-        $response = $this->client('post', 'record/telRecord', $data);
-        return $response;
-    }
-
-    /**
-     * 导出历史电话记录
-     */
-    public function telRecordHistory($data)
-    {
-        $response = $this->client('post', 'record/telRecordHistory', $data);
-        return $response;
-    }
-
-    /**
-     * 导出短信记录
-     */
-    public function sendSms($data)
-    {
-        $response = $this->client('post', 'record/sendSms', $data);
-        return $response;
-    }
-
-    /**
-     * 导出历史短信记录
-     */
-    public function sendSmsHistory($data)
-    {
-        $response = $this->client('post', 'record/sendSmsHistory', $data);
-        return $response;
-    }
-
-    /**
-     * 添加电话记录
-     */
-    public function addTelRecord($data)
-    {
-        $data = [
-            'list' => $data
-        ];
-        $response = $this->client('post', 'record/addTelRecord', $data);
-        return $response;
-    }
-
-    /**
-     * 查询客户轨迹
-     */
-    public function getTrajectory($data)
-    {
-        $response = $this->client('post', 'customer/getTrajectory', $data);
-        return $response;
-    }
-
-    /**
-     * 获取销售金额字段信息
-     */
-    public function getSalesFieldMapping()
-    {
-        $response = $this->client('get', 'sales/getSalesFieldMapping');
-        return $response;
-    }
-
-    /**
-     * 创建销售金额
-     */
-    public function addSales($data)
-    {
-        $response = $this->client('post', 'sales/addSales', $data);
-        return $response;
-    }
-
-    /**
-     * 修改销售金额
-     */
-    public function updateSales($data)
-    {
-        $response = $this->client('post', 'sales/updateSales', $data);
-        return $response;
-    }
-
-    /**
-     * 更新销售金额状态
-     */
-    public function updateStatus($data)
-    {
-        $response = $this->client('post', 'sales/updateStatus', $data);
-        return $response;
-    }
-
-    /**
-     * 查询销售金额列表
-     * @param array $data
-     */
-    public function getSales($data)
-    {
-        $response = $this->client('post', 'sales/getSales', $data);
-        return $response;
-    }
-
-    /**
-     * 查询销售金额详情
-     * @param int $saleId 销售金额的id,在创建销售金额时返回的id，或者查询列表得到id。
-     * @return string
-     */
-    public function getSalesDetail($saleId)
-    {
-        $data = [
-            'saleId' => $saleId
-        ];
-        $response = $this->client('post', 'sales/getSalesDetail', $data);
-        return $response;
-    }
 }
